c90519dbcc2a5cafea1ec793f2e16efb5b904126
kushagra kamal <kushagrakamal98@gmail.com>
2022-04-25 16:28:20 +0530

    make it better

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ const connect = ()=>{
 app.use('/login',login);
 app.use('/signup', register);
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('pages/index'));
 
 
 
