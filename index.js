290c18d19522d139321ecad517a708884e657a10
kushagra kamal <kushagrakamal98@gmail.com>
2022-04-25 16:33:05 +0530

    make it better

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const {login, register}= require("./controller/admin.controller");
+const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 5000;
 
