41611644480b7d7149f98ab7e206f87fd0abe14d
kushagra kamal <kushagrakamal98@gmail.com>
2022-04-26 10:44:50 +0530

    heroku

--- controller/product.cntrl.js
+++ controller/product.cntrl.js
@@ -1,46 +1,55 @@
 
 const express = require("express")
-const product=require("../model/product")
+const product = require("../model/product")
 
 
 const router = express.Router()
 
-router.get("",async (req,res)=>{
+router.get("", async (req, res) => {
 
     try {
         const data = await product.find().lean().exec()
         return res.status(201).send(data)
-        
+
     } catch (error) {
         return res.status(500).send(error.message)
-        
+
     }
 })
-router.post("",async(req,res)=>{
+router.get("/:id", async (req, res) => {
+    try {
+      const showArt = await product.findById(req.params.id);
+      return res.send(showArt);
+    } catch (err) {
+      return res.send(err.message);
+    }
+  });
+  
+router.post("", async (req, res) => {
 
 
     try {
         const data = await product.create(req.body)
         return res.status(201).send(data)
-        
+
     } catch (error) {
         console.log(error)
         return res.status(500).send(error.message)
-        
+
     }
 })
 
 
-router.get("/city", async (req,res) => {
-    try{
-    
-      const city = req.query.city;
-        const showArt=await product.find({city:city}).lean().exec();
- 
+router.get("/city", async (req, res) => {
+    try {
+
+        const city = req.query.city;
+        const showArt = await product.find({ city: city }).lean().exec();
+
         return res.send(showArt);
-    }catch(err){
-      return res.send(err.message);
-    } 
+    } catch (err) {
+        return res.send(err.message);
+    }
 });
 
-module.exports =router;
+module.exports = router;
