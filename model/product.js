cee0587e5fea556fed5b8099d82875bf6677470f
kushagra kamal <kushagrakamal98@gmail.com>
2022-04-26 11:38:43 +0530

    heroku

--- model/product.js
+++ model/product.js
@@ -1,4 +1,5 @@
 
+const { text } = require('express');
 const mongoose = require('mongoose');
 const productSchema = mongoose.Schema({
     name:{type:String, required:true},
@@ -6,7 +7,7 @@ const productSchema = mongoose.Schema({
     address:{type:String, required:true},
     capacity:{type:Number, required:true},
     costperday:{type:Number, required:true},
-    verified:{type:Boolean, required:true},
+    verified:{type:text, required:true},
     rating:{type:Number, required:true}
 },{
    versionKey:false,
